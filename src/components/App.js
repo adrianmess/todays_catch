03613b4a1fb214f083eb1d1d2f7ab06c4da59766
Adrian Messado <adrian.messado@gmail.com>
2018-11-08 15:49:18 -0800

    import header, inventory and order component, add component tags in App component

---
+++ src/components/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Header from './Header';
+import Order from './Order';
+import Inventory from './Inventory';
+
+class App extends React.Component {
+  render(){
+    return (
+      <div className="catch-of-the-day">
+        <div className="menu">
+          <Header />
+        </div>
+        <Inventory/>
+        <Order />
+      </div>
+    )
+
+
+  }
+}
+
+export default App;
