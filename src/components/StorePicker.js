571154d340fddcc751134b929b50ce5562c29b13
Adrian Messado <adrian.messado@gmail.com>
2018-11-08 15:18:09 -0800

    add title, input field and button to StorePicker component

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -2,7 +2,15 @@ import React from 'react';
 
 class StorePicker extends React.Component{
   render(){
-    return <p>tests</p>
+    return(
+      <React.Fragment>
+        <form className="store-selector">
+          <h2>Test</h2>
+          <input type="text" required placeholder="Store Name"></input>
+          <button type="submit">submit</button>
+        </form>
+      </React.Fragment>
+    )
   }
 }
 
