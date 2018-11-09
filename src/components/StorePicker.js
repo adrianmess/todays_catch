7978c4fc9349140f1543f4701f842393165d4289
Adrian Messado <adrian.messado@gmail.com>
2018-11-09 15:46:23 -0800

    add form with title, input and submit button, use REF in input field to reference user input

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,12 +1,20 @@
 import React from 'react';
-
+import { getFunName } from "../helpers";
 class StorePicker extends React.Component{
+  myInput = React.createRef();
+
+
   render(){
     return(
       <React.Fragment>
-        <form className="store-selector">
+        <form className="store-selector" onSubmit={this.goToStore}>
           <h2>Test</h2>
-          <input type="text" required placeholder="Store Name"></input>
+
+          <input type="text"
+            ref = {this.myInput}
+            required
+            placeholder="Store Name"
+            defaultValue={getFunName()}/>
           <button type="submit">submit</button>
         </form>
       </React.Fragment>
