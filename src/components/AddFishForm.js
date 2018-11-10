a7c389855b4c771ce7bb6ed9312fa96d9b528081
Adrian Messado <adrian.messado@gmail.com>
2018-11-09 19:58:54 -0800

    create Refs for each input, select and textarea and test via console.

--- src/components/AddFishForm.js
+++ src/components/AddFishForm.js
@@ -1,23 +1,34 @@
 import React from 'react';
 
 class AddFishForm extends React.Component {
-   createFish = (e) =>{
+
+  nameRef = React.createRef();
+  priceRef = React.createRef();
+  statusRef = React.createRef();
+  descRef = React.createRef();
+  imageRef = React.createRef();
+
+  createFish = (e) =>{
     //1. stop form from submitting
     e.preventDefault();
     console.log("Make a fish")
+    console.log(this.nameRef.value.value)
   }
+
+
+
 
   render () {
     return (
       <form className="fish-edit" onSubmit={this.createFish}>
-        <input name="name" type="text" placeholder="Name"/>
-        <input name="price" type="text" placeholder="Price"/>
-        <select name="status">
+        <input name="name" ref={this.nameRef} type="text" placeholder="Name"/>
+        <input name="price" ref={this.priceRef} type="text" placeholder="Price"/>
+        <select name="status" ref={this.statusRef}>
           <option value="available">Fresh!</option>
           <option value="unavailable">Sold Out!</option>
         </select>
-        <textarea name="desc" type="text" placeholder="Desc"/>
-        <input name="image " type="text" placeholder="Image"/>
+        <textarea name="desc" ref={this.descRef} type="text" placeholder="Desc"/>
+        <input name="image" ref={this.imageRef} type="text" placeholder="Image"/>
         <button type="submit">+ Add Fish</button>
       </form>
     )
