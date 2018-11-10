f236efee5b2b7a233e7345c60354ce6232b98e79
Adrian Messado <adrian.messado@gmail.com>
2018-11-09 19:46:04 -0800

    add onSubmit event handler to run createFish function, create createFish function and test submit with console log

--- src/components/AddFishForm.js
+++ src/components/AddFishForm.js
@@ -1,16 +1,22 @@
 import React from 'react';
 
 class AddFishForm extends React.Component {
+   createFish = (e) =>{
+    //1. stop form from submitting
+    e.preventDefault();
+    console.log("Make a fish")
+  }
+
   render () {
     return (
-      <form className="fish-edit">
+      <form className="fish-edit" onSubmit={this.createFish}>
         <input name="name" type="text" placeholder="Name"/>
         <input name="price" type="text" placeholder="Price"/>
         <select name="status">
           <option value="available">Fresh!</option>
           <option value="unavailable">Sold Out!</option>
         </select>
-        <input name="desc" type="text" placeholder="Desc"/>
+        <textarea name="desc" type="text" placeholder="Desc"/>
         <input name="image " type="text" placeholder="Image"/>
         <button type="submit">+ Add Fish</button>
       </form>
