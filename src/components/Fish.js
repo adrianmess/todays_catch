e188214746dc1697d49d4b1e7fd782c222a85ed4
Adrian Messado <adrian.messado@gmail.com>
2018-11-10 13:03:37 -0800

    use es6 destructuring to set this.prop.details... to variables. use variables for tag props and data

--- src/components/Fish.js
+++ src/components/Fish.js
@@ -2,10 +2,13 @@ import React from 'react';
 
 class Fish extends React.Component{
   render(){
+    const {image, name, price, description, status} = this.props.details;
     return(
-      <div className="single-fish">
-        +fish emoji
-      </div>
+      <li className="menu-fish">
+        <img src={image} alt={name}/>
+        <h3 className="fish-name">{name}</h3>
+        <span className="price">{price}</span>
+      </li>
 
     )
   }
