7ca2d7f6260c5dd417fac08c2802f4b4bc1496fa
Adrian Messado <adrian.messado@gmail.com>
2018-11-09 21:18:12 -0800

    wiithin addFish function, create copy of exsiting state, add new fish to fishes object with current time\/date, set new fishes object to state

--- src/components/App.js
+++ src/components/App.js
@@ -5,26 +5,31 @@ import Inventory from './Inventory';
 
 class App extends React.Component {
 
-  State = {
-        fishes:{},
-        order:{}
+  state = {
+    fishes:{},
+    order:{}
   };
 
   addFish = fish => {
-    console.log("Adding a fish!");
-  }
+    // 1. take a copy of the exsiting state
+    const fishes = { ...this.state.fishes };
+    // 2. Add our new fish to that fishes variable
+    fishes[`fish${Date.now()}`] = fish;
+    // 3. Set the fishes object to State
+    this.setState({ fishes : fishes });
+}
 
-  render(){
-    return (
-      <div className="catch-of-the-day">
-        <div className="menu">
-          <Header tagline="adrian is cool"/>
-        </div>
-        <Inventory addFish={this.addFish} />
-        <Order />
+render(){
+  return (
+    <div className="catch-of-the-day">
+      <div className="menu">
+        <Header tagline="adrian is cool"/>
       </div>
-    )
-  }
+      <Inventory addFish={this.addFish} />
+      <Order />
+    </div>
+  )
+}
 }
 
 export default App;
