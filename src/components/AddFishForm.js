af39faadeda2ca3695df355859d5669d48d26411
Adrian Messado <adrian.messado@gmail.com>
2018-11-09 16:35:03 -0800

    create AddFishForm component

---
+++ src/components/AddFishForm.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class AddFishForm extends React.Component {
+  render () {
+    return (
+      <form>
+        <p>Testing</p>
+      </form>
+    )
+  }
+}
+
+export default AddFishForm
