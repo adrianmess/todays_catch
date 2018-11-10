5a21d97cbacd6c732387d341d44f7d468c54e089
Adrian Messado <adrian.messado@gmail.com>
2018-11-09 23:04:29 -0800

    fill with test data

--- src/components/Fish.js
+++ src/components/Fish.js
@@ -3,7 +3,10 @@ import React from 'react';
 class Fish extends React.Component{
   render(){
     return(
-      
+      <div className="single-fish">
+        +fish emoji
+      </div>
+
     )
   }
 }
