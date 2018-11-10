75283536028a9e81350c4ea417b0a2a98c7f7ab6
Adrian Messado <adrian.messado@gmail.com>
2018-11-09 19:33:19 -0800

    add input fields and select options to form in AddFishForm component

--- src/components/AddFishForm.js
+++ src/components/AddFishForm.js
@@ -3,8 +3,16 @@ import React from 'react';
 class AddFishForm extends React.Component {
   render () {
     return (
-      <form>
-        <p>Testing</p>
+      <form className="fish-edit">
+        <input name="name" type="text" placeholder="Name"/>
+        <input name="price" type="text" placeholder="Price"/>
+        <select name="status">
+          <option value="available">Fresh!</option>
+          <option value="unavailable">Sold Out!</option>
+        </select>
+        <input name="desc" type="text" placeholder="Desc"/>
+        <input name="image " type="text" placeholder="Image"/>
+        <button type="submit">+ Add Fish</button>
       </form>
     )
   }
