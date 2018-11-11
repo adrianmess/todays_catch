a365057ed458238b2cd94c5df660cba81512fe28
Adrian Messado <adrian.messado@gmail.com>
2018-11-11 12:50:07 -0800

    add if statement to make sure fish is loaded before continueing

--- src/components/Order.js
+++ src/components/Order.js
@@ -5,7 +5,10 @@ class Order extends React.Component{
   renderOrder = key => {
     const fish = this.props.fishes[key];
     const count = this.props.order[key];
-    const isAvailable = fish.status === 'available';
+    const isAvailable = fish && fish.status === 'available';
+    //make sure fish is loaded before we continue
+    if(!fish) return null;
+
     if(!isAvailable){
       return (<li key={key}> Sorry {fish ? fish.name : 'fish' } is no longer available </li>);
     }
@@ -31,7 +34,7 @@ class Order extends React.Component{
       <div className="order-wrap">
         <h2>Order!!</h2>
         <div>
-          <ul>{orderIds.map(this.renderOrder)}</ul>
+          <ul className="order">{orderIds.map(this.renderOrder)}</ul>
 
           <div className="total">
             Total:
