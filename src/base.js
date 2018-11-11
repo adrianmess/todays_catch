3a4f1984bffd5db7384720e193b4a580d99da56d
Adrian Messado <adrian.messado@gmail.com>
2018-11-10 23:38:45 -0800

    remove extra curl bracket

--- src/base.js
+++ src/base.js
@@ -2,11 +2,9 @@ import Rebase from 're-base';
 import firebase from 'firebase';
 
 const firebaseApp = firebase.initializeApp({
-  {
     apiKey: "AIzaSyAsHW5eL-umK9vSxKHLJd-5xMqa_xSlKdE",
     authDomain: "todays-catch-market.firebaseapp.com",
     databaseURL: "https://todays-catch-market.firebaseio.com"
-  }
 })
 
 //create firebase bindings
