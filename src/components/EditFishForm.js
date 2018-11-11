4d99cac728015dad3fdd9f4dc6c5574452f26b26
Adrian Messado <adrian.messado@gmail.com>
2018-11-11 15:29:21 -0800

    fill out edit form with input, select and textarea tags

--- src/components/EditFishForm.js
+++ src/components/EditFishForm.js
@@ -3,7 +3,17 @@ import React from 'react';
 class EditFishForm extends React.Component{
   render() {
     return(
-      <p>Edit Fish</p>
+      <div className="fish-edit">
+      <input type="text" name="name" />
+      <input type="text" name="name" />
+      <input type="text" name="price" />
+      <select type="text" name="status">
+          <option value="available">Fresh!</option>
+          <option value="unavailable">Sold Out!</option>
+      </select>
+      <textarea name="desc" />
+      <input type="text" name="image" />
+      </div>
     )
   }
 };
