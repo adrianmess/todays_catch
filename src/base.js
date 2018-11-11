cb0abd9f831763201d524d8e5c3edcc1298a9ed1
Adrian Messado <adrian.messado@gmail.com>
2018-11-10 23:19:33 -0800

    create firebaseApp variable equal to firebase.initializeApp function with firebase config info from firebase

--- src/base.js
+++ src/base.js
@@ -1,2 +1,10 @@
 import Rebase from 're-base';
 import firebase from 'firebase';
+
+const firebaseApp = firebase.initializeApp({
+  {
+    apiKey: "AIzaSyAsHW5eL-umK9vSxKHLJd-5xMqa_xSlKdE",
+    authDomain: "todays-catch-market.firebaseapp.com",
+    databaseURL: "https://todays-catch-market.firebaseio.com"
+  }
+})
