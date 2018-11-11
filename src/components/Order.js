8bc6a3358b99dad07707e77cd06686932e972dfa
Adrian Messado <adrian.messado@gmail.com>
2018-11-10 17:19:28 -0800

    import formatPrice function, use function to display total price

--- src/components/Order.js
+++ src/components/Order.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { formatPrice } from '../helpers';
 
 class Order extends React.Component{
   render() {
@@ -18,7 +19,8 @@ class Order extends React.Component{
         <div>
           {orderIds}
           <div className="total">
-            <strong>{total}</strong>
+            Total:
+            <strong>{formatPrice(total)}</strong>
           </div>
         </div>
       </div>
