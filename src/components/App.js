6917ece48b18583af4c5bceb5b7201c007d544f0
Adrian Messado <adrian.messado@gmail.com>
2018-11-11 01:17:13 -0800

    add componentDidUpdate lifecycle method and test

--- src/components/App.js
+++ src/components/App.js
@@ -24,6 +24,10 @@ class App extends React.Component {
     });
   }
 
+  componentDidUpdate(){
+    console.log("it updated");
+  };
+
   componentWillUnmount(){
     base.removeBinding(this.ref);
   };
