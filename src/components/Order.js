64170738d0ca7abe4525fdaeb1a543109d222306
Adrian Messado <adrian.messado@gmail.com>
2018-11-10 17:15:19 -0800

    add variables equal to props, create variable equal to function that calculates price of fish and displays message if fish has been added but is no longer available

--- src/components/Order.js
+++ src/components/Order.js
@@ -3,11 +3,23 @@ import React from 'react';
 class Order extends React.Component{
   render() {
     const orderIds = Object.keys(this.props.order)
+    const total = orderIds.reduce((prevTotal, key) => {
+      const fish = this.props.fishes[key];
+      const count = this.props.order[key];
+      const isAvailable = fish && fish.status === 'available';
+      if(isAvailable) {
+        return prevTotal + (count * fish.price);
+      }
+      return prevTotal;
+    }, 0)
     return(
       <div className="order-wrap">
         <h2>Order!!</h2>
         <div>
           {orderIds}
+          <div className="total">
+            <strong>{total}</strong>
+          </div>
         </div>
       </div>
     )
