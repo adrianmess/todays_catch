69f3a5a1e3502f606e86e4c5e397a249b8d6cd6c
Adrian Messado <adrian.messado@gmail.com>
2018-11-11 13:36:14 -0800

    minor syntax fixes

--- src/components/EditFishForm.js
+++ src/components/EditFishForm.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
-class EditFishForm extends React.Component(){
+class EditFishForm extends React.Component{
   render() {
     return(
-      <p>Edit Fish</p>;
+      <p>Edit Fish</p>
     )
   }
-}
+};
 
 export default EditFishForm;
