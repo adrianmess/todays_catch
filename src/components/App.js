8b007e4355588f56bd84a779d4262bb484433a28
Adrian Messado <adrian.messado@gmail.com>
2018-11-11 01:16:31 -0800

    add componentWillUnmount function to removeBinding of firebase base this.ref reference

--- src/components/App.js
+++ src/components/App.js
@@ -17,12 +17,16 @@ class App extends React.Component {
     // alt way
     // const { params } = this.props.match;
     // this.ref = base.syncState(`${params.storeId}/fishes`, {
-    
+
     this.ref = base.syncState(`${this.props.match.params.storeId}/fishes`, {
       context: this,
       state: 'fishes'
     });
   }
+
+  componentWillUnmount(){
+    base.removeBinding(this.ref);
+  };
 
   addFish = fish => {
     // 1. take a copy of the exsiting state
@@ -31,43 +35,43 @@ class App extends React.Component {
     fishes[`fish${Date.now()}`] = fish;
     // 3. Set the fishes object to State
     this.setState({ fishes : fishes });
-}
+  }
 
-loadSampleFishes = () => {
-  this.setState({fishes : sampleFishes});
-}
+  loadSampleFishes = () => {
+    this.setState({fishes : sampleFishes});
+  }
 
-addToOrder = (key) => {
-  // 1. take a copy of this.state
-  const order = { ...this.state.order };
-  // 2. Either add to the order, or update the number in our Order
-  order[key] = order[key] + 1 || 1;
-  // 3. call setState to update our state object
-  this.setState({ order });
-}
+  addToOrder = (key) => {
+    // 1. take a copy of this.state
+    const order = { ...this.state.order };
+    // 2. Either add to the order, or update the number in our Order
+    order[key] = order[key] + 1 || 1;
+    // 3. call setState to update our state object
+    this.setState({ order });
+  }
 
-render(){
-  return (
-    <div className="catch-of-the-day">
-      <div className="menu">
-        <Header tagline="adrian is cool"/>
-        <ul className="fishes">
-          {Object.keys(this.state.fishes).map(key => <Fish
-            key={key}
-            index={key}
-            details={this.state.fishes[key]}
-            addToOrder={this.addToOrder}
-            />)}
-        </ul>
+  render(){
+    return (
+      <div className="catch-of-the-day">
+        <div className="menu">
+          <Header tagline="adrian is cool"/>
+          <ul className="fishes">
+            {Object.keys(this.state.fishes).map(key => <Fish
+              key={key}
+              index={key}
+              details={this.state.fishes[key]}
+              addToOrder={this.addToOrder}
+              />)}
+            </ul>
 
-      </div>
-      <Inventory
-        addFish={this.addFish}
-        loadSampleFishes={this.loadSampleFishes} />
-      <Order fishes={this.state.fishes} order={this.state.order} />
-    </div>
-  )
-}
-}
+          </div>
+          <Inventory
+            addFish={this.addFish}
+            loadSampleFishes={this.loadSampleFishes} />
+          <Order fishes={this.state.fishes} order={this.state.order} />
+        </div>
+      )
+    }
+  }
 
-export default App;
+  export default App;
