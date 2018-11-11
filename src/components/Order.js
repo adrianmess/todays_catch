a0fc4319acb154a7d8fcd2b1abbf9071c41f59c0
Adrian Messado <adrian.messado@gmail.com>
2018-11-10 16:04:32 -0800

    create orderIds variable equal to Object.keys of props.order. place orderIds in div, test.

--- src/components/Order.js
+++ src/components/Order.js
@@ -2,8 +2,14 @@ import React from 'react';
 
 class Order extends React.Component{
   render() {
+    const orderIds = Object.keys(this.props.order)
     return(
-      <div className="order">Order!!</div>
+      <div className="order-wrap">
+        <h2>Order!!</h2>
+        <div>
+          {orderIds}
+        </div>
+      </div>
     )
   }
 }
