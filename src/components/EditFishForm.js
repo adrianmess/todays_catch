a40442472b7bcf9d1b7906b93d0cc59898f21b17
Adrian Messado <adrian.messado@gmail.com>
2018-11-11 18:55:49 -0800

    add onclick event handler to button, set onClick to run deleteFish function, pass index prop as key to deleteFish function

--- src/components/EditFishForm.js
+++ src/components/EditFishForm.js
@@ -32,7 +32,8 @@ class EditFishForm extends React.Component{
       </select>
       <textarea name="desc" onChange={this.handleChange} value={this.props.fish.desc}/>
       <input type="text" name="image" onChange={this.handleChange} value={this.props.fish.image}/>
-      <button>Remove Fish</button>
+      {/* use inline function to run deleteFish function */}
+      <button onClick={() => this.props.deleteFish(this.props.index)}>Remove Fish</button>
       </div>
     )
   }
