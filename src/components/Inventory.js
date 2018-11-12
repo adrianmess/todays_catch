72e919776c464a716173ad61f0bc091fbac99257
Adrian Messado <adrian.messado@gmail.com>
2018-11-11 17:24:45 -0800

    pass updateFish function prop to EditFishForm

--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -10,7 +10,11 @@ class Inventory extends React.Component {
         <h2>Inventory</h2>
         {/* //for each fish render out an edit fish tag, convert to array first before mapping */}
         {Object.keys(this.props.fishes).map(key => (
-          <EditFishForm key={key} fish={this.props.fishes[key]}/>
+          <EditFishForm
+          key={key}
+          fish={this.props.fishes[key]}
+          updateFish={this.props.updateFish}
+          />
         ))}
         <AddFishForm addFish={this.props.addFish}/>
         <button onClick={this.props.loadSampleFishes}>
