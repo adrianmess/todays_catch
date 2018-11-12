30521a9c5bb2b7a32ce7e467c940215468759685
Adrian Messado <adrian.messado@gmail.com>
2018-11-11 18:50:46 -0800

    add deleteFish function as prop to Inventory component

--- src/components/App.js
+++ src/components/App.js
@@ -101,6 +101,7 @@ class App extends React.Component {
           <Inventory
             addFish={this.addFish}
             updateFish={this.updateFish}
+            deleteFish={this.deleteFish}
             loadSampleFishes={this.loadSampleFishes}
             fishes={this.state.fishes}
             />
