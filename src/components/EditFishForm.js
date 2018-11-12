5ba1824680ebd6a301e123cc80c55cc02af704b3
Adrian Messado <adrian.messado@gmail.com>
2018-11-11 16:39:34 -0800

    set value of tags to appropriate props

--- src/components/EditFishForm.js
+++ src/components/EditFishForm.js
@@ -4,15 +4,14 @@ class EditFishForm extends React.Component{
   render() {
     return(
       <div className="fish-edit">
-      <input type="text" name="name" />
-      <input type="text" name="name" />
-      <input type="text" name="price" />
+      <input type="text" name="name" value={this.props.fish.name}/>
+      <input type="text" name="price" value={this.props.fish.price}/>
       <select type="text" name="status">
           <option value="available">Fresh!</option>
           <option value="unavailable">Sold Out!</option>
       </select>
-      <textarea name="desc" />
-      <input type="text" name="image" />
+      <textarea name="desc" value={this.props.fish.desc}/>
+      <input type="text" name="image" value={this.props.fish.image}/>
       </div>
     )
   }
