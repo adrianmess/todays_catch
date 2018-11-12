512f4943d9b419b0aff2d9655901782bd164aaa5
Adrian Messado <adrian.messado@gmail.com>
2018-11-11 17:21:13 -0800

    create updateFish component that... takes a copy of current state, updates that state, then sets state

--- src/components/App.js
+++ src/components/App.js
@@ -49,6 +49,15 @@ class App extends React.Component {
     this.setState({ fishes : fishes });
   }
 
+  updateFish = (key, updatedFish) => {
+    //1. take a copy of the current state
+    const fishes = {...this.state.fishes};
+    //2. update that state
+    fishes[key] = updatedFish;
+    //3. set that to state
+    this.setState({ fishes : fishes})
+  }
+
   loadSampleFishes = () => {
     this.setState({fishes : sampleFishes});
   }
