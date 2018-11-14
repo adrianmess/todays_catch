7c447859e09f15f6696cf5e9dfb375d7cfb79bac
Adrian Messado <adrian.messado@gmail.com>
2018-11-14 12:48:04 -0800

    import TransitionGroup and CSSTransition libraries, Add CSSTransition to some li tags in renderOrder function, add replace ul with TransitionGroup

--- src/components/Order.js
+++ src/components/Order.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { formatPrice } from '../helpers';
+import { TransitionGroup, CSSTransition} from 'react-transition-group';
 
 class Order extends React.Component{
   renderOrder = key => {
@@ -10,16 +11,25 @@ class Order extends React.Component{
     if(!fish) return null;
 
     if(!isAvailable){
-      return (<li key={key}> Sorry {fish ? fish.name : 'fish' } is no longer available </li>);
+      return (
+      <CSSTransition classNames="order" key={key} timeout={{ enter:250, exit:250}}>
+      <li key={key}> Sorry {fish ? fish.name : 'fish' } is no longer available </li>
+      </CSSTransition>
+      );
     }
     return (
-      <div key={key}>
-    <li >
+ <CSSTransition classNames="order" key={key} timeout={{ enter:250, exit:250}}>
+ <li key={key}>
       {count} lbs {fish.name}
       {formatPrice(count * fish.price)}
+      <button onClick={() => this.props.removeFromOrder(key)}>
+      &times;
+      </button>
     </li>
-    <button onClick={() => (this.props.removeFromOrder(key))}>Remove From Order</button>
-      </div>
+ </CSSTransition>
+
+
+
 
   );
 };
@@ -38,7 +48,9 @@ class Order extends React.Component{
       <div className="order-wrap">
         <h2>Order!!</h2>
         <div>
-          <ul className="order">{orderIds.map(this.renderOrder)}</ul>
+          <TransitionGroup component="ul" className="order">
+          {orderIds.map(this.renderOrder)}
+          </TransitionGroup>
 
           <div className="total">
             Total:
