1b87980d6ffae9b45eab63035db7fb782fbc0a79
Adrian Messado <adrian.messado@gmail.com>
2018-11-14 16:59:24 -0800

    fix issue with with calling props

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,20 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Login = () => (
+const Login = (props) => (
     <nav className="login">
         <h2>Inventory Login</h2>
         <p>Sign in to manage your store's inventory.</p>
         <button className='github'
-        onClick={() => this.props.authenticate('Github')}>
+        onClick={() => props.authenticate('Github')}>
         Log In With Github
         </button>
         <button className='facebook'
-        onClick={() => this.props.authenticate('Facebook')}>
+        onClick={() => props.authenticate('Facebook')}>
             Log In With Github
         </button>
         <button className='twitter'
-        onClick={() => this.props.authenticate('Twitter')}>
+        onClick={() => props.authenticate('Twitter')}>
             Log In With Github
         </button>
     </nav>
