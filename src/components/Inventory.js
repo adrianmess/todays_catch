cf578a4cb259cfdf87b43c8e81e8ba6a249e8407
Adrian Messado <adrian.messado@gmail.com>
2018-11-14 16:49:59 -0800

    add signin note, add button with onClick event to run inline function of this.props.authenticate('Guthub')

--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -1,37 +1,39 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import AddFishForm from './AddFishForm';
-import EditFishForm from './EditFishForm';
+import React from "react";
+import PropTypes from "prop-types";
+import AddFishForm from "./AddFishForm";
+import EditFishForm from "./EditFishForm";
+import Login from "./Login";
 
 class Inventory extends React.Component {
   static propTypes = {
     fishes: PropTypes.object,
-   updateFish: PropTypes.func,
-   deleteFish: PropTypes.func,
-   loadSampleFishes: PropTypes.func
-  }
+    updateFish: PropTypes.func,
+    deleteFish: PropTypes.func,
+    loadSampleFishes: PropTypes.func
+  };
 
-  render () {
+  render() {
+    return <Login />;
     return (
       <div className="inventory">
         <h2>Inventory</h2>
         {/* //for each fish render out an edit fish tag, convert to array first before mapping */}
         {Object.keys(this.props.fishes).map(key => (
           <EditFishForm
-          key={key}
-          index={key}
-          fish={this.props.fishes[key]}
-          updateFish={this.props.updateFish}
-          deleteFish={this.props.deleteFish}
+            key={key}
+            index={key}
+            fish={this.props.fishes[key]}
+            updateFish={this.props.updateFish}
+            deleteFish={this.props.deleteFish}
           />
         ))}
-        <AddFishForm addFish={this.props.addFish}/>
+        <AddFishForm addFish={this.props.addFish} />
         <button onClick={this.props.loadSampleFishes}>
-        Load Sample Fishes
+          Load Sample Fishes
         </button>
       </div>
-    )
+    );
   }
 }
 
-export default Inventory
+export default Inventory;
