c57e317752b57217c383c5b83d06aeb48cdce145
Adrian Messado <adrian.messado@gmail.com>
2018-11-14 16:56:02 -0800

    add buttons with authentication propstypes for facebook and twitter, import PropTypes and define PropType for authenticate

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,10 +1,27 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const Login = () => {
+const Login = () => (
     <nav className="login">
         <h2>Inventory Login</h2>
+        <p>Sign in to manage your store's inventory.</p>
+        <button className='github'
+        onClick={() => this.props.authenticate('Github')}>
+        Log In With Github
+        </button>
+        <button className='facebook'
+        onClick={() => this.props.authenticate('Facebook')}>
+            Log In With Github
+        </button>
+        <button className='twitter'
+        onClick={() => this.props.authenticate('Twitter')}>
+            Log In With Github
+        </button>
     </nav>
-    }
+);
 
+Login.propTypes = {
+    authenticate: PropTypes.func.isRequired
+};
 
 export default Login;
